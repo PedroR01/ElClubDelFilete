aa6118c3393fa040773347eb56d6abf848a7da77
Pedro Robinet <peporobinet01@gmail.com>
2024-10-21 18:55:32 -0300

    New Section component structure

--- frontend/src/components/Button.jsx
+++ frontend/src/components/Button.jsx
@@ -2,11 +2,7 @@ export default function Button({ text, bgColor, textColor, onClick }) {
   return (
     <button
       className={
-        "bg-[" +
-        bgColor +
-        "] text-[" +
-        textColor +
-        "] font-bold py-3 px-6 text-lg rounded-lg border-2 border-[#8B2A1F] shadow-md hover:bg-[#CC8F41] hover:text-white"
+        "bg-[" + bgColor + "] text-[" + textColor + "] font-bold py-3 px-6 text-lg rounded-lg border-2 border-[#8B2A1F] shadow-md hover:bg-[#CC8F41] hover:text-white"
       }
       onClick={() => onClick}
     >
--- frontend/src/components/Section.jsx
+++ frontend/src/components/Section.jsx
@@ -7,38 +7,32 @@ export default function Section({ name, description }) {
     console.log("Contact modal open");
   };
   return (
-    <div className="min-h-screen bg-[#8B2A1F] text-white relative">
-      <div className="container mx-auto mt-8 px-4 flex">
-        <div className="flex flex-col items-start w-1/3">
+    <div className="flex min-h-screen bg-[#8B2A1F] text-white justify-self-center">
+      <ul className="columns-1 md:columns-2">
+        <li>
           <img
             src={logoIntro}
             alt="Logo club del filete"
             className="h-80 w-80 md:h-96 md:w-96 lg:h-[30rem] lg:w-[30rem] object-cover mb-4"
           />
-          <div className="flex justify-center w-full">
-            <Button
-              text="Contacto"
-              bgColor={"#DDAA58"}
-              textColor={"#8B2A1F"}
-              onClick={openContactModal}
-            />
-          </div>
-        </div>
-
-        {/* Sección para Video a la derecha */}
-        <div className="flex-grow flex flex-col items-center w-2/3">
-          <div className="mb-4">
-            <iframe
-              className="w-full h-64 md:h-96"
-              src="https://www.youtube.com/embed/PN4xt9RuV-Y"
-              title="Video"
-              frameBorder="0"
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-              allowFullScreen
-            ></iframe>
-          </div>
-        </div>
-      </div>
+          <Button
+            text="Contacto"
+            bgColor={"#DDAA58"}
+            textColor={"#8B2A1F"}
+            onClick={openContactModal}
+          />
+        </li>
+        <li className="mt-14">
+          <iframe
+            className="w-full h-64 md:h-96"
+            src="https://www.youtube.com/embed/PN4xt9RuV-Y"
+            title="Video"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+          ></iframe>
+        </li>
+      </ul>
     </div>
   );
 }
