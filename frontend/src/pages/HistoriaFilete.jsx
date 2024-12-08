fcb18414e8558aa7a7d40c8f211f7e65f8faf4ed
Juan Andres Roche <rochejuanandres@gmail.com>
2024-12-08 06:53:20 -0300

    para pullear

--- frontend/src/pages/HistoriaFilete.jsx
+++ frontend/src/pages/HistoriaFilete.jsx
@@ -11,7 +11,7 @@ export default function Historia() {
             {/* Texto para la primera mancha */}
             <div className="absolute top-[7%] left-[53%] text-white text-center w-1/3">
                 <h2 className="text-lg font-bold mb-10">Historia del Filete</h2>
-                <p class="whitespace-pre-line mb-8">
+                <p class="whitespace-pre-line">
                     El filete porteño es mucho más que un estilo decorativo; es una tradición que llevo en cada trazo y cada espiral, algo que, de alguna manera, siento que he vivido en carne propia. Esta historia empieza en las calles de Buenos Aires, a fines del Siglo XIX cuando los carros de los inmigrantes comenzaron a llenarse de color y de formas únicas. Con pinceladas audaces y detalles vibrantes, esos primeros fileteadores, en su mayoría trabajadores de origen humilde, lograron que cada carro reflejara algo propio en medio de una ciudad que no paraba de crecer. Así, nacía el filete, un arte popular que se hizo símbolo de identidad en Buenos Aires y más tarde en Latinoamérica.
                 </p>
             </div>
