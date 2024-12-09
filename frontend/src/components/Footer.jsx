51287f692fd43c4e1be1485e79f20b86e5a816c7
Pedro Robinet <peporobinet01@gmail.com>
2024-12-09 20:22:46 -0300

    Fixing footer style

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,12 +1,12 @@
 export default function Footer() {
   return (
-    <footer className="bg-[#FEFFEB] text-[#2B1F12] py-4">
+    <footer className="bg-[#FEFFEB] text-[#2B1F12] py-4 text-last">
       <div className="container mx-auto flex flex-wrap justify-between items-center text-center md:text-left">
         <div className="w-full md:w-1/3 mb-4 md:mb-0">
           <p>Realizado por <span className="font-bold">Voltio⚡</span></p>
         </div>
-        <div className="w-full md:w-1/3 mb-4 md:mb-0">
-          <p className="font-bold">Administrador</p>
+        <div className="w-full md:w-1/3 mb-4 md:mb-0 disabled">
+          <button className="font-bold opacity-65" disabled>Administrador</button>
         </div>
         <div className="w-full md:w-1/3">
           <p>Contáctanos <span className="font-bold">gpVoltio@gmail.com</span></p>
