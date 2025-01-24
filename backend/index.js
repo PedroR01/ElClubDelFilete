798a6040a3ba7760feb97ca9250b5057ce303142
Massimo <massimoparzanese@gmail.com>
2025-01-23 21:30:42 -0300

    Verified session in back

--- backend/index.js
+++ backend/index.js
@@ -219,6 +219,60 @@ app.post("/api/verify", async (req, res) => {
     return res.send({ data: true }); // Sesión verificada con éxito
   });
 
+app.get('/api/verifySecond', async (req, res) => {
+    try {
+      const accessToken = req.cookies['access_token'];
+      const refreshToken = req.cookies['refresh_token'];
+      // Verificar si existen cookies
+      if (!accessToken && !refreshToken) {
+        return res.status(401).json({ message: 'No hay sesión activa, inicie sesión nuevamente' });
+      }
+      // Intentar verificar la sesión con el access token actual
+      const { data: session, error } = await supabase.auth.getUser(accessToken);
+      if (!accessToken || error ) {
+        console.log('El token de acceso es inválido o ha expirado. Intentando refrescar...');
+  
+        if (!refreshToken) {
+          return res.status(401).json({ message: 'Token de refresco no encontrado. Inicie sesión nuevamente.' });
+        }
+  
+        // Intentar renovar el token con el refresh token
+        const { data: refreshedSession, error: refreshError } = await supabase.auth.refreshSession({
+          refresh_token: refreshToken,
+        });
+  
+        if (refreshError) {
+          return res.status(401).json({ message: 'Sesión expirada, vuelva a iniciar sesión' });
+        }
+  
+        // Guardar el nuevo access_token y refresh_token en cookies
+        res.cookie('access_token', refreshedSession.session.access_token, {
+          httpOnly: true,
+          secure: process.env.NODE_ENV === 'production',
+          maxAge: 60 * 60 * 1000,
+          sameSite: 'Strict',
+        });
+  
+        res.cookie('refresh_token', refreshedSession.session.refresh_token, {
+          httpOnly: true,
+          secure: process.env.NODE_ENV === 'production',
+          maxAge: 1700000000000,
+          sameSite: 'Strict',
+        });
+  
+        return res.json({ message: 'Sesión renovada'});
+      }
+  
+      // Si el token es válido, devolver el usuario
+      return res.json({ message: 'Sesión válida', user: session.user });
+  
+    } catch (error) {
+      console.error('Error verificando la sesión:', error);
+      res.status(500).json({ message: 'Error interno del servidor' });
+    }
+  });
+  
+
 app.post("/api/logout", async (req, res) => {
   console.log("Consulta sobre cierre")
   try{
@@ -285,7 +339,7 @@ app.get("/api/storage", async (req, res) => {
         return archivos;
       })
     );
-    console.log(subcarpetas)
+    //console.log(subcarpetas)
     // Obtener la URL pública de cada archivo
     const imagenesUrls = [];
     let i = 0;
