5a608b86e6eda4b6d82160e8668d53f4f025afbc
Pedro Robinet <peporobinet01@gmail.com>
2025-03-02 17:10:59 -0300

    testing vercel CORS config.

--- backend/middlewares/corsMiddleware.js
+++ backend/middlewares/corsMiddleware.js
@@ -4,7 +4,7 @@ const corsMiddleware = cors({
   origin: ["http://localhost:5173", "https://elclubdelfilete.com.ar"],
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
-  credentials: true, // Esto permite enviar cookies
+  // credentials: true, // Esto permite enviar cookies
 });
 
 export default corsMiddleware;
